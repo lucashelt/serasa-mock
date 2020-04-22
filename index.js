c3ffa9e8ca017e6078464ea6e27358881ad31dc1
lucashelt <lucas.helt@gmail.com>
2020-04-22 20:17:47 -0300

    serasa mock

--- index.js
+++ index.js
@@ -1,70 +1,71 @@
 const express = require('express')
-const cors = require('cors')
-const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.use(cors({ origin: '*' }))
-
-app.get('/:document', (request, response) => {
-    response.send({
-        "CPF_CNPJ": request.params.document,
-        "AlertaDocumentos":{
-           "TotalOcorrencias":0,
-           "Mensagem":"NAO CONSTAM OCORRENCIAS"
-        },
-        "PendenciasInternas":{
-           "TotalOcorrencias":0,
-           "Ocorrências": [],
-           "Mensagem":"NAO CONSTAM OCORRENCIAS"
-        },
-        "RestricoesFinanceiras":{
-           "TotalOcorrencias":0,
-           "Ocorrências": [],
-           "Mensagem":"NAO CONSTAM OCORRENCIAS"
-        },
-        "PendenciasFinanceiras":{
-           "TotalOcorrencias":0,
-           "Ocorrências": [],
-           "Mensagem":"NAO CONSTAM OCORRENCIAS"
-        },
-        "PendenciasBacen":{
-           "TotalOcorrencias":0,
-           "Ocorrências": [],
-           "Mensagem":"NAO CONSTAM OCORRENCIAS"
-        },
-        "Protestos":{
-           "TotalOcorrencias":0,
-           "Ocorrências": [],
-           "Mensagem":"NAO CONSTAM OCORRENCIAS"
-        },
-        "AcoesJudiciais":{
-           "TotalOcorrencias":0,
-           "Ocorrências": [],
-           "Mensagem":"NAO CONSTAM OCORRENCIAS"
-        },
-        "AcheiRecheque":{
-           "TotalOcorrencias":0,
-           "Ocorrências": [],
-           "Mensagem":"NAO CONSTAM OCORRENCIAS"
-        },
-        "ConvemDevedores":{
-           "TotalOcorrencias":0,
-           "Ocorrências": [],
-           "Mensagem":"NAO CONSTAM OCORRENCIAS"
-        },
-        "ParticipacoesFalencias":{
-           "TotalOcorrencias":0,
-           "Ocorrências": [],
-           "Mensagem":"NAO CONSTAM OCORRENCIAS"
-        },
-        "RiskScore":{
-        },
-        "Mensagem":"CONSULTA REALIZADA COM SUCESSO",
-        "Status":true,
-        "Transacao":{
-           "Status":true,
-           "CodigoStatus":"G000M011",
-        }
-     })
-})
-
-app.listen(5000, () => console.log('LISTENING ON PORT 5000'))
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/:document', (req, res) => res.send(
+   {
+      "CPF_CNPJ": req.params.document,
+      "AlertaDocumentos":{
+         "TotalOcorrencias":0,
+         "Mensagem":"NAO CONSTAM OCORRENCIAS"
+      },
+      "PendenciasInternas":{
+         "TotalOcorrencias":0,
+         "Ocorrências": [],
+         "Mensagem":"NAO CONSTAM OCORRENCIAS"
+      },
+      "RestricoesFinanceiras":{
+         "TotalOcorrencias":0,
+         "Ocorrências": [],
+         "Mensagem":"NAO CONSTAM OCORRENCIAS"
+      },
+      "PendenciasFinanceiras":{
+         "TotalOcorrencias":0,
+         "Ocorrências": [],
+         "Mensagem":"NAO CONSTAM OCORRENCIAS"
+      },
+      "PendenciasBacen":{
+         "TotalOcorrencias":0,
+         "Ocorrências": [],
+         "Mensagem":"NAO CONSTAM OCORRENCIAS"
+      },
+      "Protestos":{
+         "TotalOcorrencias":0,
+         "Ocorrências": [],
+         "Mensagem":"NAO CONSTAM OCORRENCIAS"
+      },
+      "AcoesJudiciais":{
+         "TotalOcorrencias":0,
+         "Ocorrências": [],
+         "Mensagem":"NAO CONSTAM OCORRENCIAS"
+      },
+      "AcheiRecheque":{
+         "TotalOcorrencias":0,
+         "Ocorrências": [],
+         "Mensagem":"NAO CONSTAM OCORRENCIAS"
+      },
+      "ConvemDevedores":{
+         "TotalOcorrencias":0,
+         "Ocorrências": [],
+         "Mensagem":"NAO CONSTAM OCORRENCIAS"
+      },
+      "ParticipacoesFalencias":{
+         "TotalOcorrencias":0,
+         "Ocorrências": [],
+         "Mensagem":"NAO CONSTAM OCORRENCIAS"
+      },
+      "RiskScore":{
+      },
+      "Mensagem":"CONSULTA REALIZADA COM SUCESSO",
+      "Status":true,
+      "Transacao":{
+         "Status":true,
+         "CodigoStatus":"G000M011",
+      }
+   }
+  ))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
